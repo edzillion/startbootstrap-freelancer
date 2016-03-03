7f9a7eda0534dce4f5b377f87ab95d5771eb8ea2
ed murphy <ed murphy>
2016-03-03 15:35:12 +0100

    changed settings to js obj

---
+++ dev.config.js
@@ -0,0 +1,8 @@
+var config = {};
+
+config.domain = "example.com";
+
+config.mailgun = {};
+config.mailgun.api_key = "key-XXXXXXXXXXXXXXXXXXXXX";
+config.mailgun.domain = "mg.example.com";
+config.mailgun.from_email = "mail@example.com";
